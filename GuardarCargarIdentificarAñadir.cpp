07341fd3f79e644f184e10e3aa520f1b22abb12a
Julen Pérez Hernández <43370446+i72pehej@users.noreply.github.com>
2018-12-12 19:47:21 +0100

    IdentificarProfesorV1 terminada

---
+++ GuardarCargarIdentificarAñadir.cpp
@@ -0,0 +1,39 @@
+//GuardarCargarIdentificarAñadir.cpp
+//Métodos de las funciones.
+
+#include "GuardarCargarIdentificarAñadir.hpp"
+
+bool Profesor::identificar(std::string usuario, std::string password){
+  std::ifstream credenciales("credenciales.bin", std::ios::binary);
+  std::string nombreProfesor, contrProfesor;
+
+  while (!credenciales.eof()) {
+    getline(credenciales, nombreProfesor, ',');
+    getline(credenciales, contrProfesor, '\n');
+
+    if ((usuario == nombreProfesor) && (password == contrProfesor)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Profesor::guardarCopia(std::string fichero){
+/*
+  for (std::list <Alumno>::iterator iAlumno = alumnos_.begin(); iAlumno != alumnos_.end(); iAlumno++) {
+
+  }
+*/
+}
+
+bool Profesor::cargarCopia(std::string fichero){
+/*
+  for (std::list <Alumno>::iterator iAlumno = alumnos_.begin(); iAlumno != alumnos_.end(); iAlumno++) {
+
+  }
+*/
+}
+
+bool Coordinador::addProfesor(std::string usuario, std::string password){
+
+}
