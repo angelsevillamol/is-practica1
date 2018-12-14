9520bcee3b52d7a1f0ebb355bc21b35db39aca0e
Daniel Ortega León <diortega89@gmail.com>
2018-12-15 00:34:41 +0100

    Funcion eliminaralumno realizada

--- basedatos.cpp
+++ basedatos.cpp
@@ -1,6 +1,7 @@
 #include "BaseDatos.hpp"
 #include "alumno.hpp"
 #include <list>
+#include <string>
 
 bool BaseDatos::anadirAlumno(std::string dni, std::string nombre, std::string apellidos, std::string telefono, std::string email, std::string direccion, std::string fecha_nacimiento, unsigned curso, unsigned grupo,bool es_lider){
   if(alumno_.size()>=150){
@@ -19,4 +20,24 @@ bool BaseDatos::anadirAlumno(std::string dni, std::string nombre, std::string ap
   a.set_es_lider(es_lider);
   alumno_.push_back(a);
   return true;
+}
+bool BaseDatos::eliminarAlumno(std::string dni){
+ buscarAlumnos("",dni,0);
+
+ std::list<alumno>::iterator i;
+
+	if(alumno_.empty()==true){
+	return -1;
+	}
+
+	for(i=alumno_.begin();i!=alumno_.end();i++){
+
+		if(i->get_dni()==dni){
+			alumno_.erase(i);
+			return 1;
+		}
+
+	}
+
+	return -2;
 }
