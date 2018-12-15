84d7cac65767121132dc4955072981be307363d9
Daniel Ortega León <diortega89@gmail.com>
2018-12-15 16:27:06 +0100

    Modficacion de la clase BaseDatos

--- BaseDatos.h
+++ BaseDatos.h
@@ -11,7 +11,7 @@ private:
   std::list <Alumno> alumno_;
 
 public:
-  bool añadirAlumno(std::string dni, std::string nombre, std::string apell, std::string telefono, std::string email, std::string dir, int curso, std::string fechaNac, int grupo, bool lider);
+  bool añadirAlumno();
   bool modificarAlumno(std:string dni, Alumno alumno);
   bool eliminarAlumno(std::string dni);
 }
